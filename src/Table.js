da859b349bec50f2c4373c25afc63f546edf7547
余珩 <1281005689@qq.com>
2018-12-03 23:10:08 +0800

    [Table] Fix header table

--- src/Table.js
+++ src/Table.js
@@ -3,10 +3,75 @@ import PropTypes from 'prop-types';
 
 class Table extends React.Component {
   render() {
-    return <table />;
+    const {
+      columns,
+      data,
+    } = this.props;
+
+    return (
+      <div className="table">
+        <div className="table-header">
+          <table className="table-content-table">
+            <colgroup>
+              {
+                columns.map((column) => (
+                  <col
+                    key={column.key}
+                    style={{ width: column.width }}
+                  />
+                ))
+              }
+            </colgroup>
+            <thead>
+              <tr>
+                {
+                  columns.map((column) => (
+                    <th key={column.key}>
+                      {column.title}
+                    </th>
+                  ))
+                }
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div className="table-body">
+          <table className="table-content-table">
+            <colgroup>
+              {
+                columns.map((column) => (
+                  <col
+                    key={column.key}
+                    style={{ width: column.width }}
+                  />
+                ))
+              }
+            </colgroup>
+            <tbody>
+              {
+                data.map((row) => (
+                  <tr key={row.key}>
+                    {
+                      columns.map((column) => (
+                        <td key={`${row.key}-${column.key}`}>
+                          {row[column.key]}
+                        </td>
+                      ))
+                    }
+                  </tr>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
   }
 }
 
-Table.propTypes = {};
+Table.propTypes = {
+  columns: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
+};
 
 export default Table;
