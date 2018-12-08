3023571663ff37d3d7525941d0dd7d109cf0fa6a
余珩 <1281005689@qq.com>
2018-12-08 16:01:02 +0800

    [TableBody] Remove colgroup and define prop types

--- src/TableBody.js
+++ src/TableBody.js
@@ -9,39 +9,28 @@ class TableBody extends React.PureComponent {
     } = this.props;
 
     return (
-      <React.Fragment>
-        <colgroup>
-          {
-            columns.map((column) => (
-              <col
-                key={column.key}
-                style={{ width: column.width }}
-              />
-            ))
-          }
-        </colgroup>
-        <tbody>
-          {
-            data.map((row) => (
-              <tr key={row.key}>
-                {
-                  columns.map((column) => (
-                    <td key={`${row.key}-${column.key}`}>
-                      {row[column.key]}
-                    </td>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </tbody>
-      </React.Fragment>
+      <tbody>
+        {
+          data.map((row) => (
+            <tr key={row.key}>
+              {
+                columns.map((column) => (
+                  <td key={`${row.key}-${column.key}`}>
+                    {row[column.key]}
+                  </td>
+                ))
+              }
+            </tr>
+          ))
+        }
+      </tbody>
     );
   }
 }
 
 TableBody.propTypes = {
-
+  columns: PropTypes.array.isRequired,
+  data: PropTypes.array.isRequired,
 };
 
 export default TableBody;
