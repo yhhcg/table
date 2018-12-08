bf1546162016511eefd91dd617b74e9a5fb8ecbc
余珩 <1281005689@qq.com>
2018-12-08 15:59:28 +0800

    [core] TableColgroup

---
+++ src/TableColgroup.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class TableColgroup extends React.PureComponent {
+  render() {
+    const {
+      columns,
+    } = this.props;
+
+    return (
+      <colgroup>
+        {
+          columns.map((column) => (
+            <col
+              key={column.key}
+              style={{ width: column.width }}
+            />
+          ))
+        }
+      </colgroup>
+    );
+  }
+}
+
+TableColgroup.propTypes = {
+  columns: PropTypes.array.isRequired,
+};
+
+export default TableColgroup;
